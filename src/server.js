dc5ab85695c71e91bdc5aacac6231b1dcf8acbb1
tung17122k <sanking1712@gmail.com>
2025-03-10 10:04:16 +0700

    setup with mongodb driver

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const connection = require('./config/database');
 const fileUpload = require('express-fileupload');
+const { MongoClient } = require('mongodb');
 
 
 const configViewEngine = require("./config/viewEngine");
@@ -32,7 +33,30 @@ app.use('/v1/api/', apiRoutes);
 (async () => {
     try {
         // test connection
-        await connection();
+
+        // await connection();
+
+        // using mongodb driver
+        const url = process.env.DB_HOST_WITH_DRIVER;
+        const client = new MongoClient(url);
+        // db name
+        const dbName = process.env.DB_NAME;
+
+        await client.connect();
+        console.log('Connected successfully to server');
+        const db = client.db(dbName);
+        const collection = db.collection('customers');
+        await collection.insertOne({
+            name: "test",
+            email: "test@gmail.com",
+            address: "Ha Noi",
+            phone: "0392293758"
+        });
+        let a = await collection.findOne({ name: "test" });
+        console.log(">>>check a", a);
+
+
+
         app.listen(port, hostname, () => {
             console.log(`Backend app listening on port ${port}`)
         })
