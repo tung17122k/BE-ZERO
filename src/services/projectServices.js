73f0a1967d314af079509bb6512e19ba64460ba0
tung17122k <sanking1712@gmail.com>
2025-03-14 14:41:34 +0700

    add users to proj

--- src/services/projectServices.js
+++ src/services/projectServices.js
@@ -1,11 +1,33 @@
 const Project = require("../models/project");
 
 
-const createProjectService = async (projectData) => {
+const createProjectService = async (data) => {
     try {
-        if (projectData.type === 'EMPTY-PROJECT') {
+        if (data.type === 'EMPTY-PROJECT') {
             let result = await Project.create(projectData);
             return result;
+        }
+
+        if (data.type === "ADD-USERS") {
+            let myProject = await Project.findById(data.projectId).exec();
+
+            // Duyệt qua từng user trong mảng usersArr
+            for (const userId of data.usersArr) {
+                // Kiểm tra user đã tồn tại trong project chưa
+                const isUserExists = myProject.usersInfor.some(existingUser =>
+                    existingUser.toString() === userId
+                );
+
+                // Chỉ thêm user nếu chưa tồn tại
+                if (!isUserExists) {
+                    myProject.usersInfor.push(userId);
+                }
+            }
+
+
+            let newResult = await myProject.save();
+            console.log(newResult);
+            return newResult
         }
     } catch (error) {
         return null
